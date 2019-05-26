b5facf0567064ad01a25faa39431a202a8632687
gorden5566 <gorden5566@126.com>
2019-05-26 19:27:31 +0800

    init

---
+++ src/main/java/com/gorden5566/demo/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package com.gorden5566.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author gorden5566
+ * @date 2019-05-26
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+}
